4d94dd31957223cefd9bc406c5bceca989e3d9f2
Shahil Saha <71847625+shahilsaha05uk@users.noreply.github.com>
2024-03-02 20:22:59 +0000

    updates 2024

---
+++ assets/js/Swiper.js
@@ -0,0 +1,32 @@
+﻿var swiper = new Swiper('.mySwiper', {
+    loop: true,
+    autoHeight: true, // Enable auto height
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    on: {
+        slideChange: function () {
+            // Pause all YouTube iframes when sliding to a new slide
+            var iframes = document.querySelectorAll('.swiper-slide iframe');
+            iframes.forEach(function (iframe) {
+                // Using postMessage to send a command to the YouTube API
+                iframe.contentWindow.postMessage(JSON.stringify({
+                    event: 'command',
+                    func: 'pauseVideo',
+                    args: []
+                }), '*');
+            });
+        },
+        // Include other event handlers if necessary
+    },
+});
