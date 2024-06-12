4a0a332641702b7691aa568ccfb5857c61383194
Shahil Saha <s015695k@student.staffs.ac.uk>
2024-06-12 20:40:44 +0100

    fixes

---
+++ assets/js/mongodb.js
@@ -0,0 +1,20 @@
+﻿const {MongoClient} = require('mongodb');
+
+async function main()
+{
+    const uri = "mongodb+srv://shahilsaha05uk:<Staffs-05>@codicide.gmtvxiz.mongodb.net/?retryWrites=true&w=majority&appName=Codicide";
+    
+    const client = new MongoClient(uri);
+    
+    try {
+        await client.connect();
+    }
+    catch (e){
+        console.error(e);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
