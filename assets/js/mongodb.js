711472e21052782f52d3595464e680baf0c30113
Shahil Saha <s015695k@student.staffs.ac.uk>
2024-07-03 12:06:29 +0100

    s

--- assets/js/mongodb.js
+++ assets/js/mongodb.js
@@ -1,13 +1,16 @@
 ﻿const {MongoClient} = require('mongodb');
 
+// setting up the connection
 async function main()
 {
-    const uri = "mongodb+srv://shahilsaha05uk:<Staffs-05>@codicide.gmtvxiz.mongodb.net/?retryWrites=true&w=majority&appName=Codicide";
+    const uri = "mongodb+srv://shahilsaha05uk:Stoke-05@testcluster.yhjcfef.mongodb.net/?retryWrites=true&w=majority&appName=TestCluster";
     
     const client = new MongoClient(uri);
     
     try {
         await client.connect();
+        
+        await listDatabases(client);
     }
     catch (e){
         console.error(e);
@@ -18,3 +21,8 @@ async function main()
 }
 
 main().catch(console.error);
+
+// getting the database
+async function listDatabases(client){
+
+}
